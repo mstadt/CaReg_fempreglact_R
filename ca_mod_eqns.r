ce62d02311422f02b0df7ab9712c32faf67dcc1b
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2023-07-11 11:38:36 -0400

    set up model equations and parameter functions

---
+++ ca_mod_eqns.r
@@ -0,0 +1,13 @@
+ca_mod_eqns <- function(Time, State, Pars) {
+    # simulation settings
+    
+    # state varables
+    # amt_PTHg  -- amount of PTH in the parathyroid gland pool
+    # amt_PTHp -- amount of PTH in plasma
+    # amt_Cap  -- amount of Ca in plasma
+    # amt_D3p  -- amount of 1,25(OH)2D3 in plasma
+    # amt_NCaf -- amount of calcium in rapidly exchangeable pool
+
+    with(as.list(c(State, Pars)), {
+    })
+}
---
+++ set_params.r
@@ -0,0 +1,6 @@
+set_params <- function(sexORrep) {
+    # set parameters for model
+    # sexORrep -- male, female, preg, lact options
+
+    # start with only male parameter values
+}
