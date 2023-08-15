f6d600df1a30a088dac70d995c6ceca68361f0ce
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2023-08-15 11:26:16 -0400

    add male and variable options

--- compute_ss.r
+++ compute_ss.r
@@ -1,11 +1,37 @@
-compute_ss <- function(X) {
-    # initial guess values
-    # NOTE: currently female
-    amt_PTHg0 <- 35.167
-    PTHp_con0 <- 6.288
-    Cap_con0 <- 1.179
-    D3p_con0 <- 84.38
-    NCaf0 <- 1.626
+compute_ss <- function(X, optvals) {
+    sexORrep <- optvals$sexORrep
+    varname <- optvals$var_out
+
+    if (sexORrep == 'male') {
+        modeqns <- ca_mod_eqnsMorris_male_all
+        amt_PTHg0 <- 34.986
+        PTHp_con0 <- 6.279
+        Cap_con0 <- 1.179
+        D3p_con0 <- 153.688
+        NCaf0 <- 1.626
+    } else if (sexORrep == 'female') {
+        modeqns <- ca_mod_eqnsMorris_male_all
+        amt_PTHg0 <- 35.167
+        PTHp_con0 <- 6.288
+        Cap_con0 <- 1.179
+        D3p_con0 <- 84.38
+        NCaf0 <- 1.626
+    } else if (sexORrep == 'preg') {
+        modeqns <- ca_mod_eqnsMorris_preglact_all
+        amt_PTHg0 <- 24.269
+        PTHp_con0 <- 13.469
+        Cap_con0 <- 1.179
+        D3p_con0 <- 269.108
+        NCaf0 <- 1.625
+    } else if (sexORrep == 'lact') {
+        modeqns <- ca_mod_eqnsMorris_preglact_all
+        amt_PTHg0 <- 23.445
+        PTHp_con0 <- 13.247
+        Cap_con0 <- 1.178
+        D3p_con0 <- 329.178
+        NCaf0 <- 1.624
+    }
+
 
     init_guess <- c(amt_PTHg = amt_PTHg0,
                     PTHp_con = PTHp_con0,
@@ -14,16 +40,16 @@ compute_ss <- function(X) {
                     amt_NCaf = NCaf0)
 
     one_par <- function(i) {
-        ST1 <- runsteady(init_guess, time = c(0,5000), func = ca_mod_eqnsMorris_male_all,
+        ST1 <- runsteady(init_guess, time = c(0,5000), func = modeqns,
                             parms = X[i, ])
-        ST <- stodes(ST1$y, times = 0, func = ca_mod_eqnsMorris_male_all,
+        ST <- stodes(ST1$y, times = 0, func =modeqns,
                             parms = X[i, ], rtol = 1e-5, atol = 1e-7)
         # attributes(ST) # could use this to debug and see what attributes are!
         check_ss1 <- attributes(ST)$steady # this can check for steady state
         if (!check_ss1) {
             print('trying larger tol')
             print(i)
-            ST2 <- stodes(ST1$y, times = 0, func = ca_mod_eqnsMorris_male_all,
+            ST2 <- stodes(ST1$y, times = 0, func = modeqns,
                             parms = X[i, ], rtol = 1e-3, atol = 1e-3)
             check_ss2 <- attributes(ST2)$steady
             if (!check_ss2) {
@@ -41,7 +67,23 @@ compute_ss <- function(X) {
     res_per_state <- aperm(res_per_par)
 
     #var_out = "PTHp_con"
-    vals <- res_per_state[,2]
+    if (varname == 'amt_PTHg') {
+        varid <- 1
+    } else if (varname == 'PTHp_con') {
+       varid <- 2
+    } else if (varname == 'Cap_con') {
+       varid <- 3
+    } else if (varname == 'D3p_con') {
+       varid <- 4
+    } else if (varname == 'amt_NCaf') {
+       varid <- 5
+    } else {
+       varid <- 0
+       temp <- sprintf('varname: %s ', varname)
+       print(temp)
+       print('varname not found!')
+    }
+    vals <- res_per_state[, varid]
 
     return(vals)
 }
--- runMorrisSS.r
+++ runMorrisSS.r
@@ -13,15 +13,12 @@ if (sexORrep == 'male') {
     # to get testpars, parsbinf, parsbsup
     source("set_morris_all_mf.r")
     source("ca_mod_eqnsMorris_male_all.r")
-    #modeqns <- ca_mod_eqnsMorris_male_all
 } else if (sexORrep == 'female') {
     source("set_morris_all_mf.r")
     source("ca_mod_eqnsMorris_male_all.r")
-    #modeqns <- ca_mod_eqnsMorris_male_all # same for male and female
 } else if (sexORrep == 'preg') {
     source("set_morris_all_preglact.r")
     source("ca_mod_eqnsMorris_preglact_all.r")
-    #modeqns <- ca_mod_eqnsMorris_preglact_all
 } else if (sexORrep == 'lact') {
     source("set_morris_all_preglact.r")
     source("ca_mod_eqnsMorris_preglact_all.r")
@@ -37,8 +34,8 @@ rval = 100
 startall <- Sys.time()
 # PTHp_con
 print('start PTHp morris')
-startPTHp <- Sys.time()
-optsvals <- list(
+starttemp <- Sys.time()
+optsvals_PTHp <- list(
         sexORrep = sexORrep,
         var_out = "PTHp_con"
         )
@@ -50,12 +47,67 @@ x_PTHp <- morris(model = compute_ss,
                                     grid.jump = 1),
                     binf = parsbinf,
                     bsup = parsbsup,
-                    scale = TRUE
+                    scale = TRUE,
+                    optvals = optsvals_PTHp
                     )
 
-endPTHp <- Sys.time()
-print(difftime(endPTHp, startPTHp, unit = "mins"))
+endtemp <- Sys.time()
+print(difftime(endtemp, starttemp, unit = "mins"))
 
 # Cap_con
+print('start Cap morris')
+starttemp <- Sys.time()
+optsvals_Cap <- list(
+        sexORrep = sexORrep,
+        var_out = "Cap_con"
+        )
+x_Cap <- morris(model = compute_ss,
+            factors = testpars,
+            r = rval,
+            design = list(type = "oat",
+                            levels = 10,
+                            grid.jump = 1),
+            binf = parsbinf,
+            bsup = parsbsup,
+            scale = TRUE,
+            optvals = optsvals_Cap
+            )
+endtemp <- Sys.time()
+print(difftime(endtemp, starttemp, unit = "mins"))
 
 # D3p_con
+print('start D3p morris')
+starttemp <- Sys.time()
+optsvals_D3p <- list(
+        sexORrep = sexORrep,
+        var_out = "D3p_con"
+        )
+x_D3p <- morris(model = compute_ss,
+            factors = testpars,
+            r = rval,
+            design = list(type = "oat",
+                            levels = 10,
+                            grid.jump = 1),
+            binf = parsbinf,
+            bsup = parsbsup,
+            scale = TRUE,
+            optvals = optsvals_D3p
+            )
+endtemp <- Sys.time()
+print(difftime(endtemp, starttemp, unit = "mins"))
+
+end_all <- Sys.time()
+print(difftime(end_all, startall, units = "mins"))
+
+save_info = 1
+if (save_info) {
+    today <- Sys.Date()
+    fname <- paste0(today,
+                    "_CaMorrisAnalysisSS_",
+                    "_sexORrep-", sexORrep,
+                    ".RData"
+                    )
+    save.image(fname)
+    print("results saved to:")
+    print(sprintf("%s",fname))
+}
